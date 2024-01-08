09c43a3d161198e52b513d2ddfe8c85656d1a283
SutonToch <77286229+SutonToch@users.noreply.github.com>
2024-01-08 21:35:16 +0100

    minor progress yet again
    add icons, remove socials from hero since it would only be frontend mentor and github (footer should be sufficient for that), minor style tweaks

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,7 @@
 import './../styles/Hero.css'
 import Me from './../assets/me.jpg'
+import About_Me_Icon from './../assets/icons/user_light.png'
+import Contact_Icon from './../assets/icons/address-book_light.png'
 
 interface HeroText {
   greeting:String,
@@ -17,26 +19,23 @@ export default function Hero(props:HeroProps) {
   return (
     <div className="hero">
       <div className="grid-container">
-        <div className="img-social-container">
-          <div className="img-container">
-            <img src={Me} alt=""/>
-          </div>
-          <div className="social-container">
-            social icons
-          </div>
+        <div className="img-container">
+          <img src={Me} alt=""/>
         </div>
         <div className="content-container">
           <h1>{props.text_data.greeting}</h1>
-          <p>... ...</p>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, quia. Consequatur dolorum hic optio aut possimus provident, ipsam perspiciatis nisi velit impedit accusamus voluptatibus quae. Dolorem nesciunt doloremque aliquid perspiciatis deserunt, esse libero, mollitia praesentium voluptates at quisquam aperiam dolores quaerat quod voluptatum, magnam saepe hic maxime quae debitis neque.</p>
         </div>
         <a className="hero-btn" href="#">
           <div className="hero-btn--content">
-            icon & {props.text_data.btn_about}
+            <img src={About_Me_Icon} alt=""/>
+            <p>{props.text_data.btn_about}</p>
           </div>
         </a>
         <a className="hero-btn" href="#">
           <div className="hero-btn--content">
-            icon & {props.text_data.btn_contact}
+            <img src={Contact_Icon} alt=""/>
+            <p>{props.text_data.btn_contact}</p>
           </div>
         </a>
       </div>
