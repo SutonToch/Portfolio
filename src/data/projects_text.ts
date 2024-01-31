bf17211f67c7b4614ba9816ec74e8286736d0567
SutonToch <77286229+SutonToch@users.noreply.github.com>
2024-01-31 14:45:39 +0100

    UPDATE project short text
    add whitespaces at the end, for correct separation

--- src/data/projects_text.ts
+++ src/data/projects_text.ts
@@ -36,8 +36,8 @@ export const projects_text_data = {
                 ],
                 liveURL: "https://sutontoch.github.io/interactive-card-details-form/",
                 short: [
-                    "Ein kürzeres 'Frontend Mentor'-Projekt, bei dem man eine Komponente zur Eingabe von Kreditkartendaten erstellt und diese interaktiv auf der Seite anzeigen lässt.", 
-                    "Dies ist das Projekt, bei dem ich ein tieferes Verständnis dafür gewonnen habe, wie die Positionierung mit inset/top/bottom/left/right funktioniert. Zuvor wusste ich nur grob, wie es funktioniert, und habe es immer durch ausprobieren hinbekommen, dass alles so angezeigt wurde, wie ich es wollte. Jetzt könnte ich es tatsächlich jemandem erklären. In diesem Projekt habe ich auch die regulären Ausdrücke aus meiner Studienzeit wieder aufgegriffen.", 
+                    "Ein kürzeres 'Frontend Mentor'-Projekt, bei dem man eine Komponente zur Eingabe von Kreditkartendaten erstellt und diese interaktiv auf der Seite anzeigen lässt. ", 
+                    "Dies ist das Projekt, bei dem ich ein tieferes Verständnis dafür gewonnen habe, wie die Positionierung mit inset/top/bottom/left/right funktioniert. Zuvor wusste ich nur grob, wie es funktioniert, und habe es immer durch ausprobieren hinbekommen, dass alles so angezeigt wurde, wie ich es wollte. Jetzt könnte ich es tatsächlich jemandem erklären. In diesem Projekt habe ich auch die regulären Ausdrücke aus meiner Studienzeit wieder aufgegriffen. ", 
                     "Wenn Sie daran interessiert sind, sehen Sie es sich kurz an und versuchen Sie, es kaputt zu machen. Ich habe mir ziemlich viel Mühe gegeben, um sicherzustellen, dass es keine falschen Eingaben gibt. Lassen Sie es mich wissen, wenn Sie etwas finden 🙂."
                 ]
             },
@@ -52,8 +52,8 @@ export const projects_text_data = {
                 ],
                 liveURL: "https://sutontoch.github.io/calculator-app/",
                 short: [
-                    "Eine Taschenrechner-Webanwendung. Dies ist das Projekt, bei dem ich meine JavaScript- und Grid-Layout-Fähigkeiten getestet habe.", 
-                    "Ich habe ziemlich viel Arbeit in dieses Projekt gesteckt, mit ein paar zusätzlichen Funktionen mit denen ich über das Ziel hinausgegangen bin. Zum Beispiel habe ich eine eigene und sichere eval-Funktion entwickelt, und man kann die Tastatur für Eingaben in den Taschenrechner verwenden. Die eval-Funktion wurde ziemlich kompliziert, aber ich bin stolz darauf. Ich habe die Anwendung auch ausgiebig getestet, so dass es keine Möglichkeit geben sollte, sie kaputt zu machen.", 
+                    "Eine Taschenrechner-Webanwendung. Dies ist das Projekt, bei dem ich meine JavaScript- und Grid-Layout-Fähigkeiten getestet habe. ", 
+                    "Ich habe ziemlich viel Arbeit in dieses Projekt gesteckt, mit ein paar zusätzlichen Funktionen mit denen ich über das Ziel hinausgegangen bin. Zum Beispiel habe ich eine eigene und sichere eval-Funktion entwickelt, und man kann die Tastatur für Eingaben in den Taschenrechner verwenden. Die eval-Funktion wurde ziemlich kompliziert, aber ich bin stolz darauf. Ich habe die Anwendung auch ausgiebig getestet, so dass es keine Möglichkeit geben sollte, sie kaputt zu machen. ", 
                     "Ich bin mir bewusst, dass viele Portfolios einen Taschenrechner enthalten, aber ich denke, dass dieses Projekt etwas Besonderes ist, weil es zeigt, dass ich bereit und in der Lage bin, die Extrameile zu gehen. Werfen Sie also einen Blick darauf, wenn Sie interessiert sind 🙂."
                 ]
             },
@@ -71,8 +71,8 @@ export const projects_text_data = {
                 ],
                 liveURL: "",
                 short: [
-                    "Nun, hier geht es um das Projekt, das Sie gerade sehen.", 
-                    "Ich habe eine Menge Arbeit in dieses Projekt gesteckt, und es war eine lange Reise. Dies ist nicht einmal die erste Version meines Portfolios, denn ich war mit der ersten Version einfach nicht zufrieden.",
+                    "Nun, hier geht es um das Projekt, das Sie gerade sehen. ", 
+                    "Ich habe eine Menge Arbeit in dieses Projekt gesteckt, und es war eine lange Reise. Dies ist nicht einmal die erste Version meines Portfolios, denn ich war mit der ersten Version einfach nicht zufrieden. ",
                     "Ich bin mir bewusst, dass es etwas seltsam ist, sein Portfolio-Projekt in sein Portfolio aufzunehmen, aber ich denke, dass mein Weg, mein Denkprozess und meine Erfahrungen es wert sind, hier erwähnt zu werden. Wenn Sie Zeit und Lust haben, mehr zu erfahren, können Sie gerne einen Blick darauf werfen 🙂."
                 ]
             }
@@ -93,8 +93,8 @@ export const projects_text_data = {
                 ],
                 liveURL: "https://sutontoch.github.io/Klick/",
                 short: [
-                    "A short Browsergame, where boxes appear that you have to click to get points. The goal is simply to get as many points as possible.", 
-                    "After learning JavaScript, I wanted to put my knowledge into practice, and what better option than a Browsergame? For an added challenge, I decided to use Typescript to get familiar with it, and simply try it out. Initially I wanted this Browsergame to be a little more than it turned out to be, but I decided that it served it's purpose and my time was better spend elsewhere.", 
+                    "A short Browsergame, where boxes appear that you have to click to get points. The goal is simply to get as many points as possible. ", 
+                    "After learning JavaScript, I wanted to put my knowledge into practice, and what better option than a Browsergame? For an added challenge, I decided to use Typescript to get familiar with it, and simply try it out. Initially I wanted this Browsergame to be a little more than it turned out to be, but I decided that it served it's purpose and my time was better spend elsewhere. ", 
                     "It's still fun though! If you have a few minutes of spare time, consider trying it yourself. There are a few tricks to increase your score.. I'd be interested if you're able to get past 100 or even 500 score 🙂."
                 ]
             },
@@ -112,8 +112,8 @@ export const projects_text_data = {
                 ],
                 liveURL: "https://sutontoch.github.io/interactive-card-details-form/",
                 short: [
-                    "A shorter 'Frontend Mentor'-Project where you build a component to enter credit card details and display them interactivly on the page.", 
-                    "This is the project where I gained a deeper understanding of how positioning works with inset/top/bottom/left/right. Before I roughly knew what it does and used trial-and-error to get everything to display how I wanted it. Now I could actually explain it to someone else. In this project I also revisited Regular Expressions from my college time.", 
+                    "A shorter 'Frontend Mentor'-Project where you build a component to enter credit card details and display them interactivly on the page. ", 
+                    "This is the project where I gained a deeper understanding of how positioning works with inset/top/bottom/left/right. Before I roughly knew what it does and used trial-and-error to get everything to display how I wanted it. Now I could actually explain it to someone else. In this project I also revisited Regular Expressions from my college time. ", 
                     "If you're interested, take a quick look and try to break it. I put quite some effort into making sure there are no possible wrong inputs. Let me know if you find something 🙂."
                 ]
             },
@@ -128,8 +128,8 @@ export const projects_text_data = {
                 ],
                 liveURL: "https://sutontoch.github.io/calculator-app/",
                 short: [
-                    "A Calculator Web App. This is the project where I tested my JavaScript and Grid-Layout Skills.", 
-                    "I put quite some effort into this project, with a few extra features, that went beyond the necessary features. For example, I made my own secure eval-function and you can use your keyboard to do inputs for the calculator. The eval-function got pretty complicated, but I'm proud of it. I also did alot of testing, so there shouldn't be a way to break it.", 
+                    "A Calculator Web App. This is the project where I tested my JavaScript and Grid-Layout Skills. ", 
+                    "I put quite some effort into this project, with a few extra features, that went beyond the necessary features. For example, I made my own secure eval-function and you can use your keyboard to do inputs for the calculator. The eval-function got pretty complicated, but I'm proud of it. I also did alot of testing, so there shouldn't be a way to break it. ", 
                     "I'm aware that many portfolios feature a calculator, but I'd like to think that this one is special, because it shows that I'm willing and able to go the extra mile. So, take a look if you're interested 🙂."
                 ]
             },
@@ -147,8 +147,8 @@ export const projects_text_data = {
                 ],
                 liveURL: "",
                 short: [
-                    "Well, this is about the project that you are looking at right now.", 
-                    "I've put alot of work into this, and it was quite the journey. This isn't even the first version of my portfolio, because I just wasn't happy with how the first one turned out.", 
+                    "Well, this is about the project that you are looking at right now. ", 
+                    "I've put alot of work into this, and it was quite the journey. This isn't even the first version of my portfolio, because I just wasn't happy with how the first one turned out. ", 
                     "I'm aware that putting your portfolio project into your portfolio is kind of weird, but I think my journey, thought process and lessons learned are worth mentioning here. If you have the time and want to know more, feel free to take a look 🙂."
                 ]
             }
